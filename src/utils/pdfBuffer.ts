3372a4d4af22ce09ea65db4519f7558c146fc651
hariom21623 <self06311@gmail.com>
2026-05-30 22:18:01 +0530

    update pdfbuffer

--- src/utils/pdfBuffer.ts
+++ src/utils/pdfBuffer.ts
@@ -1,11 +1,29 @@
 export const htmlToPdfBuffer = async (html: string) => {
-  const { default: puppeteer } = await import('puppeteer');
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
+  const isProduction = process.env.IS_RENDER === 'true';
+
+  let browser;
+
+  if (isProduction) {
+    const chromium = await import('@sparticuz/chromium');
+    const puppeteer = await import('puppeteer-core');
+
+    browser = await puppeteer.default.launch({
+      args: chromium.default.args,
+      executablePath: await chromium.default.executablePath(),
+      headless: true,
+    });
+  } else {
+    const puppeteer = await import('puppeteer-core');
+
+    browser = await puppeteer.default.launch({
+      executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+  }
+
   const page = await browser.newPage();
-  await page.setContent(html);
+  await page.setContent(html, { waitUntil: 'load' });
   const pdfBuffer = await page.pdf({ format: 'A4' });
   await browser.close();
   return pdfBuffer;
